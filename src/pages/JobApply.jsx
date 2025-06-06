a7874b017eb7e52eb2edd3179e5d84b0f321f5a4
Fuad Hasan <fuadhasan3503@gmail.com>
2025-06-06 11:42:02 +0600

    install axios and store job-aplication data to DB

--- src/pages/JobApply.jsx
+++ src/pages/JobApply.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useParams } from "react-router";
 import useAuth from "../hocks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
   const { id: jobId } = useParams();
@@ -8,7 +10,7 @@ const JobApply = () => {
 
   console.log(jobId, user);
 
-  const handleApplyFormSubmit = e => {
+  const handleApplyFormSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const linkedIn = form.linkedIn.value;
@@ -16,22 +18,63 @@ const JobApply = () => {
     const resume = form.resume.value;
 
     console.log(linkedIn, github, resume);
-  }
+    const application = {
+      jobId,
+      applicant: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+
+    axios
+      .post("http://localhost:3000/applications", application)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your application has been submitted",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="my-10">
-      <h3 className="text-4xl mb-5">Apply For this job:<Link to={`/jobs/${jobId}`}> details</Link> </h3>
+      <h3 className="text-4xl mb-5">
+        Apply For this job:<Link to={`/jobs/${jobId}`}> details</Link>{" "}
+      </h3>
       <form onSubmit={handleApplyFormSubmit}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          
           <label className="label">LinkedIn Link</label>
-          <input type="url" name="linkedIn" className="input" placeholder="LinkedIn profile link" />
+          <input
+            type="url"
+            name="linkedIn"
+            className="input"
+            placeholder="LinkedIn profile link"
+          />
 
           <label className="label">Github Link</label>
-          <input type="url" name="github" className="input" placeholder="Github Profile Link" />
+          <input
+            type="url"
+            name="github"
+            className="input"
+            placeholder="Github Profile Link"
+          />
 
           <label className="label">Resume Link</label>
-          <input type="url" name="resume" className="input" placeholder="Resume Link" />
+          <input
+            type="url"
+            name="resume"
+            className="input"
+            placeholder="Resume Link"
+          />
 
           <input type="submit" name="btn" id="" className="btn" />
         </fieldset>
